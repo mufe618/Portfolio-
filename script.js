b0134586ccc68e5bcb729d42af6071988732b186
mufe618 <khansahilali430@gmail.com>
2026-03-03 14:10:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function OpenGmail() {
+    window.location.href = "mailto:Mufeali325@gmail.com";
+}
+ function OpenGitHub() {
+    window.location.href = "https://github.com/Mufe618";    
+
+}
+ function OpenLinkedIn() {
+    window.open("https://www.linkedin.com/in/mufeali/", "_blank");
+}
